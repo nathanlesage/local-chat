f13b30be17433f82508cefd67af20558cf2f2fb8
Hendrik Erz <hendrik@zettlr.com>
2024-01-23 12:28:45 +0100

    fix: Prepare release script

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -3,11 +3,13 @@
 CURRENT_VERSION="v$(cat package.json | jq -r '.version')"
 
 echo "The current version for LocalChat is $CURRENT_VERSION."
-echo "Which version should be updated to? Please provide in the format vX.Y.Z"
+echo ""
+echo "Which version should be updated to? Please provide in the format X.Y.Z"
+echo "DO NOT ADD THE v IN FRONT OF THE VERSION STRING!"
 
 read NEW_VERSION
 
-echo "Will bump the version for LocalChat to $NEW_VERSION. Continue?"
+echo "Will bump the version for LocalChat to v$NEW_VERSION. Continue?"
 
 # Give the user a last chance to redo if something went wrong
 select yn in "Continue" "Abort"; do
@@ -18,12 +20,12 @@ select yn in "Continue" "Abort"; do
 done
 
 echo "Bumping version in package.json ..."
-jq ".version = \"$NEW_VERSION\"" package.json > package.json
+echo "$(jq ".version = \"$NEW_VERSION\"" package.json)" > package.json
 echo "Committing changes..."
 git add package.json
-git commit -m "chore: Bump version to $NEW_VERSION"
+git commit -m "chore: Bump version to v$NEW_VERSION"
 git push
 echo "Tagging commit ..."
-git tag -a $NEW_VERSION -m "Release version $NEW_VERSION"
+git tag -a "v$NEW_VERSION" -m "Release version v$NEW_VERSION"
 git push origin $NEW_VERSION
 echo "Done."
