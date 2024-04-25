73561f6051ff3db465cb818f4eda9ac22a46b2eb
Hendrik Erz <hendrik@zettlr.com>
2024-04-25 20:44:12 +0200

    fix: Edge case in GGUF model name extraction

--- src/main-window/util/get-model-name.ts
+++ src/main-window/util/get-model-name.ts
@@ -8,7 +8,9 @@ import { ModelDescriptor } from 'src/main/ModelManager'
  * @return  {string}                  The model's name
  */
 export function getModelName (model: ModelDescriptor): string {
-  if (model.metadata?.general.name === undefined) {
+  // Either if the provided name is undefined, or if the authors just added a
+  // period or a space as a name (looking at you, QuantFactory's Llama3 Q4_K_M)
+  if (model.metadata?.general.name === undefined || model.metadata.general.name.length < 3) {
     return model.name
   } else {
     return model.metadata.general.name
